1479cc665637930fddf831af3f62f9840cfe2643
Yusuf Soylu <yusufsoylu1974@gmail.com>
2025-10-19 20:26:42 +0300

    odevvvsad

--- js/task-3.js
+++ js/task-3.js
@@ -3,7 +3,7 @@ function getElementWidth(content, padding, border) {
   const paddingWidth = parseFloat(padding);
   const borderWidth = parseFloat(border);
   const totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
-  console.log(totalWidth);
+  return totalWidth;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
